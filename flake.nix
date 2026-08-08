8667ae89d9eb15c0e302d522afecc9ec0f0f148f
Jan Van Sweevelt <vansweej@gmail.com>
2026-08-08 10:27:55 +0200

    chore: add nixpkgs-fmt to devshell and normalize flake formatting

--- flake.nix
+++ flake.nix
@@ -9,65 +9,66 @@
 
   outputs =
     inputs:
-    with inputs;
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 
-        rustVersion = "latest";
-        rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default;
+          rustVersion = "latest";
+          rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default;
 
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        };
-
-        parlang = rustPlatform.buildRustPackage {
-          pname = manifest.name;
-          version = manifest.version;
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
           };
 
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ pkgs.openssl.dev ];
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        };
-      in
-      rec {
-        packages = {
-          parlang = parlang;
-          default = packages.parlang;
-        };
+          parlang = rustPlatform.buildRustPackage {
+            pname = manifest.name;
+            version = manifest.version;
+            src = ./.;
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            rustToolchain
-            pkgs.cargo-deny
-            pkgs.cargo-edit
-            pkgs.cargo-tarpaulin
-            pkgs.cargo-watch
-            pkgs.cargo-outdated
-            pkgs.cargo-update
-            pkgs.git
-            pkgs.openssl
-            pkgs.pkg-config
-            pkgs.rust-analyzer
-          ];
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
 
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl.dev ];
             PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
           };
-        };
-      }
-    );
+        in
+        rec {
+          packages = {
+            parlang = parlang;
+            default = packages.parlang;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              rustToolchain
+              pkgs.cargo-deny
+              pkgs.cargo-edit
+              pkgs.cargo-tarpaulin
+              pkgs.cargo-watch
+              pkgs.cargo-outdated
+              pkgs.cargo-update
+              pkgs.git
+              pkgs.nixpkgs-fmt
+              pkgs.openssl
+              pkgs.pkg-config
+              pkgs.rust-analyzer
+            ];
+
+            env = {
+              # Required by rust-analyzer
+              RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+              PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            };
+          };
+        }
+      );
 }
